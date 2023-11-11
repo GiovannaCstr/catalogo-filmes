936be9a4d76e42b7834aa953c07df3f1aef7c3d5
unknown <gpereirac2001@gmail.com>
2023-11-11 14:04:10 -0300

    adicionando busca por nome e busca por id

--- src/controller/filmeController.js
+++ src/controller/filmeController.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { salvar, listarTodos } from "../repository/filmeRepository.js";
+import { salvar, listarTodos, buscarPorNome, buscarPorId } from "../repository/filmeRepository.js";
+
 
 const endpoints = Router();
+
 
 endpoints.post('/filme', async (req, resp) => {
     let filme = req.body;
@@ -9,9 +11,25 @@ endpoints.post('/filme', async (req, resp) => {
     resp.send(r);
 })
 
+
 endpoints.get('/filme', async (req, resp) => {
     let r = await listarTodos();
     resp.send(r);
 })
+
+
+endpoints.get('/filme/busca', async (req, resp) => {
+    let nome = req.query.nome;
+    let r = await buscarPorNome(nome);
+    resp.send(r);
+})
+
+
+endpoints.get('/filme/:id', async (req, resp) => {
+    let id = req.params.id;
+    let r = await buscarPorId(id);
+    resp.send(r);
+})
+
 
 export default endpoints;
--- src/repository/filmeRepository.js
+++ src/repository/filmeRepository.js
@@ -1,5 +1,6 @@
 import conexao from './connection.js';
 
+
 export async function salvar(filme) {
     let comando = `
         INSERT INTO tb_filmes (nome_filme, sinopse, avaliacao, dt_lancamento, bt_disponivel, img_filme) 
@@ -12,6 +13,7 @@ export async function salvar(filme) {
     filme.id = info.insertId;
     return filme;
 }
+
 
 export async function listarTodos() {
     let comando = `
@@ -29,3 +31,43 @@ export async function listarTodos() {
 
     return lista;
 }
+
+
+export async function buscarPorNome(nome) {
+    let comando = `
+        SELECT  id_filme        as id,
+                nome_filme      as nome,
+                sinopse         as sinopse,
+                avaliacao       as avaliacao,
+                dt_lancamento   as lancamento,
+                bt_disponivel   as disponivel
+        FROM    tb_filmes
+        WHERE   nome_filme like ?
+    `
+
+    let resp = await conexao.query(comando, ['%'+nome+'%']);
+    let lista = resp[0];
+
+    return lista;
+}
+
+
+export async function buscarPorId(id) {
+    let comando = `
+        SELECT  id_filme        as id,
+                nome_filme      as nome,
+                sinopse         as sinopse,
+                avaliacao       as avaliacao,
+                dt_lancamento   as lancamento,
+                bt_disponivel   as disponivel,
+                img_filme       as imagem
+        FROM    tb_filmes
+        WHERE   id_filme = ?
+    `
+
+    let resp = await conexao.query(comando, [id]);
+    let lista = resp[0];
+
+    return lista[0];
+}
+
